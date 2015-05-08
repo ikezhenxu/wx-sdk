c883fa8e5656853d157bbbcd17d87b6dafdf743c
kezhenxu <ChineseKezhenxu@163.com>
2015-05-08 22:16:19 +0800

    authority

--- src/test/TestCases.java
+++ src/test/TestCases.java
@@ -1,6 +1,7 @@
 package test;
 
-import huilai.kezhenxu.material.WxNewsGet;
+import huilai.kezhenxu.basic.WxAccessTokenKeeper;
+import huilai.kezhenxu.material.WxMaterialList;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -13,7 +14,9 @@ public class TestCases {
 	static String accessToken = "miX6yKQ1g93r0Aq_fqblmJgSNWVkVp-UG1Zwsh7soh5i8k1dOi4h49eVyuxtjl2fvU_1PwTLCjuMsaAGxgGpm66QwnktFWaQ0gpaLQFhEAE";
 
 	public static void main ( String[] args ) throws IOException, NoSuchAlgorithmException {
-		WxNewsGet theGet = new WxNewsGet ();
-		theGet.getByMediaId ( accessToken, "o-bA-oihGFg78tKEUlUz8Krdu2WNX-rhCdX3zMATEDk" );
+		System.out.println ( new WxMaterialList ()
+				                     .list ( "news",
+				                             0, 20, WxAccessTokenKeeper.getDefaultInstance () )
+				                     .toJSONString () );
 	}
 }
